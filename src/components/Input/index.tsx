76a90111e091f1f10af8c76802eab13975f55f02
johnnysiegl <johsiegl@gmail.com>
2021-11-28 18:57:51 -0300

    Update project

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -2,15 +2,21 @@ import {
   useEffect,
   useRef,
   useState,
-  useCallback,
+  useCallback
 } from 'react';
 
 import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+interface InputProps {
+  name: string;
+  icon?: new () => React.Component<any, any>;
+  placeholder: string;
+}
+
+const Input = ({ name, icon: Icon, ...rest }: InputProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
---
+++ src/types.ts
@@ -0,0 +1,8 @@
+export interface IFood {
+  id: number;
+  image: string;
+  name: string;
+  description: string;
+  available: boolean;
+  price: number;
+}
